1f13fef4dcdc8458dcc56cf305aef282414d6859
wiris123 <wiris123@naver.com>
2018-08-24 22:02:13 +0900

    에러정리

--- src/controller/BbsDAO.java
+++ src/controller/BbsDAO.java
@@ -15,45 +15,40 @@ import javax.sql.DataSource;
 import dto.BoardDTO;
 
 public class BbsDAO {
-	Connection con; //오라클 서버와 연결할때 사용
-	PreparedStatement psmt;//오라클 서버와 쿼리전송 역활
+	Connection con; // 오라클 서버와 연결할때 사용
+	PreparedStatement psmt;// 오라클 서버와 쿼리전송 역활
 	ResultSet rs;
-	
-	public BbsDAO() 
-	{
+
+	public BbsDAO() {
 		try {
-			Context ctx = new InitialContext(); 
-			DataSource source = 
-			  (DataSource)
-			  ctx.lookup("java:comp/env/jdbc/myoracle");
-			
+			Context ctx = new InitialContext();
+			DataSource source = (DataSource) ctx.lookup("java:comp/env/jdbc/myoracle");
+
 			con = source.getConnection();
 			System.out.println("DBCP연결성공");
-		}
-		catch(Exception e) {
+		} catch (Exception e) {
 			System.out.println("DBCP연결실패");
 			e.printStackTrace();
-		}	
+		}
 	}
+
 	public void close() {
 		try {
-			if(rs!=null) rs.close();
-			if(psmt!=null) psmt.close();
-			if(con!=null) con.close();
-		}
-		catch(Exception e) {
+			if (rs != null)
+				rs.close();
+			if (psmt != null)
+				psmt.close();
+			if (con != null)
+				con.close();
+		} catch (Exception e) {
 			System.out.println("자원반납시 예외발생");
 			e.printStackTrace();
 		}
 	}
-	
-	public int getTotalRecordCount(Map<String,Object> param) {
-		int totalCount=-1;
-<<<<<<< HEAD
-		
-=======
 
->>>>>>> refs/remotes/origin/underwearRun
+	public int getTotalRecordCount(Map<String, Object> param) {
+
+		int totalCount = -1;
 		String query = "select count(*) from multiboard where 1=1 and b_id=? ";
 		if (param.get("Word") != null) {
 			if (param.get("Column").equals("both")) {
@@ -62,36 +57,30 @@ public class BbsDAO {
 			} else {
 				query += " and " + param.get("Column") + " " + " LIKE '%" + param.get("Word") + "%' ";
 			}
-<<<<<<< HEAD
-		}
-=======
 		}
 
-
->>>>>>> refs/remotes/origin/underwearRun
-		try {
-			psmt = con.prepareStatement(query);
-			psmt.setString(1, param.get("b_id").toString());
-			rs=psmt.executeQuery();
-			if(rs.next()) {
-				totalCount=rs.getInt(1);
+			try {
+				psmt = con.prepareStatement(query);
+				psmt.setString(1, param.get("b_id").toString());
+				rs = psmt.executeQuery();
+				if (rs.next()) {
+					totalCount = rs.getInt(1);
+				}
+				
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
+			return totalCount;
 		}
-		catch(Exception e) {
-			e.printStackTrace();
-		}
-		System.out.println(totalCount);
-		return totalCount;
-	}
-	
-	//게시판리스트가져오기
-	public List<BoardDTO> selectList(Map<String,Object> param){
+
+	// 게시판리스트가져오기
+	public List<BoardDTO> selectList(Map<String, Object> param) {
 		System.out.println("게시판리스트가져오기중");
 		BoardDTO dto;
-		List<BoardDTO> list=new Vector<BoardDTO>();
+		List<BoardDTO> list = new Vector<BoardDTO>();
 		String query = "SELECT * FROM (SELECT e.*, rownum rnum FROM (SELECT * FROM multiboard WHERE 1=1 and b_id=?";
 		System.out.println(param.get("Word"));
-		System.out.println("컬럼은:"+param.get("Column"));
+		System.out.println("컬럼은:" + param.get("Column"));
 		if (param.get("Word") != null) {
 			if (param.get("Column").equals("both")) {
 				query += " and " + "title LIKE '%" + param.get("Word") + "%' " + " OR " + " contents LIKE '%"
@@ -102,34 +91,34 @@ public class BbsDAO {
 		}
 		query += ") e) where rNum BETWEEN ? AND ?";
 		try {
-			String b_id = (String)param.get("b_id");
+			String b_id = (String) param.get("b_id");
 			psmt = con.prepareStatement(query);
 			psmt.setString(1, param.get("b_id").toString());
 			psmt.setString(2, param.get("start").toString());
 			psmt.setString(3, param.get("end").toString());
-			System.out.println(query+b_id);
+			System.out.println(query + b_id);
 			rs = psmt.executeQuery();
-			while(rs.next()) {
-				System.out.println("드러왔다"+rs.getString("num"));
+			while (rs.next()) {
+				System.out.println("드러왔다" + rs.getString("num"));
 				dto = new BoardDTO();
 				dto.setNum(rs.getString("NUM"));
 				dto.setName(rs.getString("NAME"));
 				dto.setTitle(rs.getString("TITLE"));
 				dto.setRegidate(rs.getDate("REGIDATE"));
 				dto.setViewcnt(rs.getString("VIEWCNT"));
-				
+
 				list.add(dto);
 			}
-		}
-		catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return list;
 	}
-	//게시판글쓰기
+
+	// 게시판글쓰기
 	public int write(BoardDTO dto) {
 		System.out.println("게시판글쓰기중");
-		int affected=-1;
+		int affected = -1;
 		String query = "insert into multiboard values(board_seq.nextval, '짐배', ?, ?, ?, sysdate, 0, null, ?, null)";
 		try {
 			psmt = con.prepareStatement(query);
@@ -138,59 +127,59 @@ public class BbsDAO {
 			psmt.setString(3, dto.getContents());
 			psmt.setString(4, dto.getB_id());
 			affected = psmt.executeUpdate();
-		}
-		catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 		return affected;
 	}
-	//게시판글지우기
+
+	// 게시판글지우기
 	public int delete2(String[] num) {
 		System.out.println("게시판글지우기중");
 		int affected = 0;
 		System.out.println(num.length);
 		try {
 			String query = "delete from multiboard where num in (?";
-			for(int i=1;i<num.length;i++) {
-				query+=",?";
+			for (int i = 1; i < num.length; i++) {
+				query += ",?";
 			}
 			query += ")";
-			psmt = con.prepareStatement(query);		
-			for(int i=0;i<num.length;i++) {
-				psmt.setString(i+1, num[i]);
+			psmt = con.prepareStatement(query);
+			for (int i = 0; i < num.length; i++) {
+				psmt.setString(i + 1, num[i]);
 			}
-			
-			System.out.println(query); 
+
+			System.out.println(query);
 			affected = psmt.executeUpdate();
-		}
-		catch(Exception e) {
+		} catch (Exception e) {
 			System.out.println("delete_board중 예외발생");
 			e.printStackTrace();
 		}
 		return affected;
 	}
-	//삭제용 메소드
+
+	// 삭제용 메소드
 	public int delete(String num) {
 		int affected = 0;
 		try {
 			String query = "delete from multiboard where num=?";
-			
-			psmt = con.prepareStatement(query);			
+
+			psmt = con.prepareStatement(query);
 			psmt.setString(1, num);
-			 
+
 			affected = psmt.executeUpdate();
 		}
-		
-		catch(Exception e) {
+
+		catch (Exception e) {
 			System.out.println("delete_board중 예외발생");
 			e.printStackTrace();
 		}
-		System.out.println("term_name"+num);
-		return affected;	
+		System.out.println("term_name" + num);
+		return affected;
 	}
-	
-	//내용을 가져올 메소드
+
+	// 내용을 가져올 메소드
 	public BoardDTO contents(String num) {
 		System.out.println("내용가져오는중");
 		BoardDTO dto = new BoardDTO();
@@ -206,8 +195,7 @@ public class BbsDAO {
 				dto.setTitle(rs.getString("title"));
 				dto.setViewcnt(rs.getString("viewcnt"));
 			}
-		}
-		catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return dto;
