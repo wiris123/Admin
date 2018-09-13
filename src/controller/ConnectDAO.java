1042741d29be58cd09d9646a600852a4627b9eb8
wiris123 <wiris123@naver/com>
2018-09-13 18:30:03 +0900

    페이지관리 날림

--- src/controller/ConnectDAO.java
+++ src/controller/ConnectDAO.java
@@ -47,68 +47,121 @@ public class ConnectDAO {
 			e.printStackTrace();
 		}
 	}
-	
-	//모든 날짜별 리스트를 가져오는 명령어
-	public List<VisitorDTO> selectList(String start, String end){
+
+	// 모든 날짜별 리스트를 가져오는 명령어
+	public List<VisitorDTO> selectList(String start, String end) {
 		VisitorDTO dto;
 		List<VisitorDTO> list = new Vector<VisitorDTO>();
-		//db에서는 날짜를 아무 식으로나 입력해도 상관없지만 jdbc를 통한 연결을 할떄는 date 타입으로 맞춰줘야한다
+		// db에서는 날짜를 아무 식으로나 입력해도 상관없지만 jdbc를 통한 연결을 할떄는 date 타입으로 맞춰줘야한다
 		SimpleDateFormat parse = new SimpleDateFormat("YYYY-MM-dd");
 		Date date1 = Date.valueOf(start);
 		Date date2 = Date.valueOf(end);
-		
+
 		String query = "SELECT * FROM"
-	    +" (SELECT TO_CHAR(TO_DATE(?, 'YY-MM-dd') + LEVEL - 1, 'YY-MM-dd') as dates FROM DUAL CONNECT BY TO_DATE(?, 'YY-MM-dd') + LEVEL - 1 < TO_DATE(?, 'YY-MM-dd')+1) d"
-	    +" left outer join"
-	    +" (select TO_CHAR(visit_date, 'YY-MM-DD') as Vd, count(TO_CHAR(visit_date, 'YY-MM-DD'))as val from visitor group by TO_CHAR(visit_date, 'YY-MM-DD') ) v on"
-	    +" d.DATES = v.vD order by d.dates";
-			
+				+ " (SELECT TO_CHAR(TO_DATE(?, 'YY-MM-dd') + LEVEL - 1, 'YY-MM-dd') as dates FROM DUAL CONNECT BY TO_DATE(?, 'YY-MM-dd') + LEVEL - 1 < TO_DATE(?, 'YY-MM-dd')+1) d"
+				+ " left outer join"
+				+ " (select TO_CHAR(visit_date, 'YY-MM-DD') as Vd, count(TO_CHAR(visit_date, 'YY-MM-DD'))as val from visitor group by TO_CHAR(visit_date, 'YY-MM-DD') ) v on"
+				+ " d.DATES = v.vD order by d.dates";
+
 		try {
-			//System.out.println("구하는중:"+query);
+			// System.out.println("구하는중:"+query);
 			psmt = con.prepareStatement(query);
 			psmt.setDate(1, date1);
 			psmt.setDate(2, date1);
 			psmt.setDate(3, date2);
 			rs = psmt.executeQuery();
-			while(rs.next()) {
+			while (rs.next()) {
 				dto = new VisitorDTO();
-				//date 타입을 가져올떄는 4자리로 가져오지 않고 2자리만 보내준다는것 발견 to_char 를 통해 4자리 문자열로 변경하거나
-				//아니면 앞에20을 미리 붙여준다
+				// date 타입을 가져올떄는 4자리로 가져오지 않고 2자리만 보내준다는것 발견 to_char 를 통해 4자리 문자열로 변경하거나
+				// 아니면 앞에20을 미리 붙여준다
 				String comdate = "20";
 				comdate += rs.getString("dates");
-				//System.out.println(comdate);
-				comdate = comdate.replace("-", ","); //- 를 , 으로 바꾼다.
+				// System.out.println(comdate);
+				comdate = comdate.replace("-", ","); // - 를 , 으로 바꾼다.
 				dto.setVisit_date(comdate);
 				dto.setVal(rs.getInt(3));
-				
+
 				list.add(dto);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
+
+	// 모든 날짜별
+	public VisitorValDTO getTotVisitor() {
+		VisitorValDTO dto = new VisitorValDTO();
+		String query = "select sum(val), ROUND(AVG(val), 2) from (select count(TO_CHAR(visit_date, 'YYYY-MM-DD'))as val from visitor group by TO_CHAR(visit_date, 'YYYY-MM-DD'))";
+		try {
+			psmt = con.prepareStatement(query);
+			rs = psmt.executeQuery();
+			if (rs.next()) {
+				dto.setTot(rs.getInt(1));
+				dto.setAvg(rs.getDouble(2));
+			}
+			rs.close();
+			psmt.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return dto;
+	}
+
+	// 오늘 접속자수
+	public int todayVisitor() {
+		int today = 0;
+		String query = "select count(to_char(visit_date, 'YY-MM-dd')) from visitor group by to_char(visit_date, 'YY-MM-dd')  having to_char(visit_date, 'YY-MM-dd') =  to_char(sysdate, 'YY-MM-dd')";
+		try {
+			psmt = con.prepareStatement(query);
+			rs = psmt.executeQuery();
+			if (rs.next()) {
+				today = rs.getInt(1);
+			}
+			rs.close();
+			psmt.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return today;
+	}
+
+	// 어제 접속자수
+	public int yesterdayVisitor() {
+		int yesterday = 0;
+		String query = "select count(to_char(visit_date, 'YY-MM-dd')) from visitor group by to_char(visit_date, 'YY-MM-dd')  having to_char(visit_date, 'YY-MM-dd') =  to_char(sysdate-1, 'YY-MM-dd')";
+		try {
+			psmt = con.prepareStatement(query);
+			rs = psmt.executeQuery();
+			if (rs.next()) {
+				yesterday = rs.getInt(1);
+			}
+			rs.close();
+			psmt.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return yesterday;
+	}
+	
+	//한달 평균구하기
+	public VisitorValDTO monthly() {
+		VisitorValDTO mon= new VisitorValDTO();
+		//혹시 없을경우를 위한 오류처리용
+
+		String query = "select sum(count(to_char(visit_date, 'YY-MM-dd'))), avg(count(to_char(visit_date, 'YY-MM-dd'))) from visitor group by to_char(visit_date, 'YY-MM-dd')  having to_char(visit_date, 'YY-MM-dd') between add_months(sysdate,-1) and to_char(sysdate, 'YY-MM-dd')";
+		try {
+			psmt = con.prepareStatement(query);
+			rs = psmt.executeQuery();
+			if(rs.next()) {
+				mon.setTot(rs.getInt(1));
+				mon.setAvg(rs.getDouble(2));
 			}
 		}
 		catch(Exception e) {
 			e.printStackTrace();
 		}
-		return list;
+		return mon;
 	}
-	
-	//모든 날짜별 
-	public VisitorValDTO getTotVisitor() {
-		VisitorValDTO dto = new VisitorValDTO();
-		String query = "select sum(val), ROUND(AVG(val), 2) from (select count(TO_CHAR(visit_date, 'YYYY-MM-DD'))as val from visitor group by TO_CHAR(visit_date, 'YYYY-MM-DD'))";
-		try {
-		psmt = con.prepareStatement(query);
-		rs = psmt.executeQuery();
-		if(rs.next()) {
-			dto.setTot(rs.getInt(1));
-			dto.setAvg(rs.getDouble(2)); 
-		}
-		rs.close();
-		psmt.close();
-		}
-		catch(Exception e) {
-			e.printStackTrace();
-		}
-		
-		return dto;
-	}
-
 }
